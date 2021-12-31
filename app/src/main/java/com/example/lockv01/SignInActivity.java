afc5bf7d910b1657da8aa727e6323c0cda29da86
Guntur Ramadhan <48126283+StromR@users.noreply.github.com>
2021-12-31 20:04:39 +0700

    Authentication
    Sign up & Sign in authentication using firebase

--- app/src/main/java/com/example/lockv01/SignInActivity.java
+++ app/src/main/java/com/example/lockv01/SignInActivity.java
@@ -1,19 +1,104 @@
 package com.example.lockv01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignInActivity";
+    public FirebaseAuth mAuth;
+    EditText editTextEmail1, editTextPassword1;
+    TextView errorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
 
+        editTextEmail1 = findViewById(R.id.editTextEmail);
+        editTextPassword1 = findViewById(R.id.editTextPassword);
+        errorView = findViewById(R.id.textView7);
+
     }
+
+    public void signinOnClick(View view) {
+
+            if (editTextEmail1.getText().toString().contentEquals("")) {
+
+
+                errorView.setText("Email cant be empty");
+
+
+            } else if (editTextPassword1.getText().toString().contentEquals("")) {
+
+                errorView.setText("Password cant be empty");
+
+            } else {
+
+
+                mAuth.signInWithEmailAndPassword(editTextEmail1.getText().toString(), editTextPassword1.getText().toString())
+                        .addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    if (user != null) {
+                                        if (user.isEmailVerified()) {
+
+
+                                            System.out.println("Email Verified : " + user.isEmailVerified());
+                                            Intent HomeActivity = new Intent(SignInActivity.this, MainActivity.class);
+                                            setResult(RESULT_OK, null);
+                                            startActivity(HomeActivity);
+                                            SignInActivity.this.finish();
+
+
+                                        } else {
+
+                                            Toast.makeText(SignInActivity.this, "Please Verify your EmailID and SignIn.",
+                                                    Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    if (task.getException() != null) {
+                                        errorView.setText(task.getException().getMessage());
+                                    }
+
+                                }
+
+                            }
+                        });
+
+
+            }
+
+
+        };
+
 
     public void signupTextClick(View view) {
         Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
--- app/src/main/java/com/example/lockv01/SignUpActivity.java
+++ app/src/main/java/com/example/lockv01/SignUpActivity.java
@@ -1,17 +1,128 @@
 package com.example.lockv01;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
+    public FirebaseAuth mAuth;
+    EditText editTextEmail, editTextPassword;
+    TextView errorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        errorView = findViewById(R.id.textView6);
+
     }
+
+        public void signupOnClick(View view) {
+
+           if (editTextEmail .getText().toString().contentEquals("")) {
+
+
+                Toast.makeText(this, "Email can not be empty", Toast.LENGTH_LONG).show();
+
+
+            } else if (editTextPassword.getText().toString().contentEquals("")) {
+
+
+                Toast.makeText(this, "Password can not be empty", Toast.LENGTH_LONG).show();
+
+
+            }
+            else {
+
+
+                mAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString()).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            try {
+                                if (user != null)
+                                    user.sendEmailVerification()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()) {
+                                                        Log.d(TAG, "Email sent.");
+
+                                                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                                                                SignUpActivity.this);
+
+                                                        // set title
+                                                        alertDialogBuilder.setTitle("Please Verify Your EmailID");
+
+                                                        // set dialog message
+                                                        alertDialogBuilder
+                                                                .setMessage("A verification Email Is Sent To Your Registered EmailID, please click on the link and Sign in again!")
+                                                                .setCancelable(false)
+                                                                .setPositiveButton("Sign In", new DialogInterface.OnClickListener() {
+                                                                    public void onClick(DialogInterface dialog, int id) {
+
+                                                                        Intent signInIntent = new Intent(SignUpActivity.this, SignInActivity.class);
+                                                                        SignUpActivity.this.finish();
+                                                                    }
+                                                                });
+
+                                                        // create alert dialog
+                                                        AlertDialog alertDialog = alertDialogBuilder.create();
+
+                                                        // show it
+                                                        alertDialog.show();
+
+
+                                                    }
+                                                }
+                                            });
+
+                            } catch (Exception e) {
+                                errorView.setText(e.getMessage());
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            if (task.getException() != null) {
+                                errorView.setText(task.getException().getMessage());
+                            }
+
+                        }
+
+                    }
+                });
+
+            }
+
+        }
 }
